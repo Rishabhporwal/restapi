afcfa4338ea0e0c86283572347d759e2d594bf1b
Rishabh Porwal <rishabhporwal95@gmail.com>
2019-10-13 05:46:23 +0530

    login functionality and jwt tokens added

--- app.js
+++ app.js
@@ -4,56 +4,59 @@ const mongoose = require("mongoose");
 const path = require("path");
 const multer = require("multer");
 const feedRoutes = require("./routes/feed");
+const authRoutes = require("./routes/auth");
+
 const app = express();
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
-  res.setHeader("Access-Control-Allow-headers", "*");
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+    res.setHeader("Access-Control-Allow-headers", "*");
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
 });
 
 const fileStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "./images");
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.originalname);
-  }
+    destination: (req, file, cb) => {
+        cb(null, "./images");
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    }
 });
 
 const fileFilter = (req, file, cb) => {
-  if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
+    if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
 };
 
 //app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
 app.use(
-  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+    multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
 );
 app.use("/images", express.static(path.join(__dirname, 'images')));
 
 app.use("/feed", feedRoutes);
+app.use("/auth", authRoutes);
 
 app.use((error, req, res, next) => {
-  const status = error.statusCode || 500;
-  const message = error.message;
-  res.status(status).json({ message: message });
+    const status = error.statusCode || 500;
+    const message = error.message;
+    const data = error.data;
+    res.status(status).json({ message: message, message: message, data: data });
 });
 
 mongoose
-  .connect(
-    "mongodb+srv://rishabh:rishabh12345@cluster0-edqhg.mongodb.net/restapi?retryWrites=true&w=majority",
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  )
-  .then(result => {
-    app.listen(8080);
-  })
-  .catch(err => {
-    console.log(err);
-  });
+    .connect(
+        "mongodb+srv://rishabh:rishabh12345@cluster0-edqhg.mongodb.net/restapi?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true }
+    )
+    .then(result => {
+        app.listen(8080);
+    })
+    .catch(err => {
+        console.log(err);
+    });
